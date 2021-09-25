7ccdb532afd478eafff585541997f9b2561626f4
wuchunlong0 <wcl6005@163.com>
2021-09-25 09:05:56 +0800

    del

--- courseinfo/data/init.sh
+++
@@ -1,2 +0,0 @@
-cp /home/www/ecustCourseInfo/src/courseinfo/data/syncdb.py /home/www/ecustCourseInfo/src/courseinfo/syncdb.py
-crontab /home/www/ecustCourseInfo/src/courseinfo/data/crontab.txt
--- docker-config/start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-[ -f /home/www/ecustCourseInfo/src/courseinfo/data/init.sh ] && sh /home/www/ecustCourseInfo/src/courseinfo/data/init.sh
-
-nginx
-/usr/bin/supervisord -c /etc/supervisor/supervisord.conf
